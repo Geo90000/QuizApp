9efcc0b1d17d0f1a0cd8556e08121708406099d6
geofs <geo.f.shahem@gmail.com>
2020-12-06 11:12:21 +0100

    commit initial

--- app/src/main/java/com/example/ip2/MainActivity.java
+++ app/src/main/java/com/example/ip2/MainActivity.java
@@ -2,13 +2,47 @@ package com.example.ip2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
+
+   Button buttonMenuSpiel;
+   TextView timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonMenuSpiel = findViewById(R.id.btnMenuSpiel);
+
+        buttonMenuSpiel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+
+        timer = findViewById(R.id.txtTimer);
+
+        new CountDownTimer(30000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                timer.setText("" + millisUntilFinished / 1000);
+            }
+            public void onFinish() {
+                timer.setText("0:00");
+            }
+        }.start();
     }
 }
